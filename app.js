77eee69cdefc5356c174a9fa1874fc92e881a6b7
JakeMaz <jakemazanowski@gmail.com>
2020-12-16 11:17:33 -0500

    app.js tested in postman. setup done

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// develop branch
+require('dotenv').config();
+
+const express = require('express');
+const db = require('./db');
+const app = express();
+
+app.use(require('./middleware/headers'));
+app.use(express.json())
+
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname);
+app.get('/', (req,res)=> res.render('index'));
+
+db.authenticate()
+.then(()=>db.sync())
+.then(()=>{
+    app.listen(process.env.PORT, ()=> console.log(`[SERVER:] App is listening on Port ${process.env.PORT}`))
+})
+.catch((err)=>{
+    console.log('[SERVER:] Server Crashed');
+    console.error(err);
+})
